e898540a83f27eb6cc52ada63395e4f2f1d8eddd
ShadoLight <1608425735@qq.com>
2023-05-24 20:53:50 +0800

    find&insert

--- request.cpp
+++ request.cpp
@@ -1,5 +1,7 @@
 #include "student.h"
 
+ofstream testout(file_path + "requests.out");
+
 void preload()
 {
 	cout << "正在载入地图信息..." << endl;
@@ -8,9 +10,38 @@ void preload()
 	stuinit();
 }
 
+Student stu;
+
+void login()
+{
+	string username = "yzh";
+	int id = stus_dict[username];
+	stu = stus[id];
+
+}
+
+ifstream info_reqs(file_path + "requests.in");
+
+void stutest()
+{
+	int num_reqs;
+	Activity x;
+
+	info_reqs >> num_reqs;
+
+	for (int i = 1; i <= num_reqs; i++)
+	{
+		x.read(info_reqs);
+		bool flag = stu.find_acti(x);
+		testout << i << flag << endl;
+	}
+	stu.textout(testout);
+}
+
 int main()
 {
 	preload();
+	login();
 	stutest();
 	return 0;
 }
